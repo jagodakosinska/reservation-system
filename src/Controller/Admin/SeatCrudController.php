96486885cdbfb5942fc9e8d0d8dce353ad6b3806
jagodakosinska <jagoda.kosinska87@gmail.com>
2023-05-26 10:54:09 +0200

    feat(AdminCrud): SeatCrudController - customize view by adding new fields, relation and messenger to generate seats

--- src/Controller/Admin/SeatCrudController.php
+++ src/Controller/Admin/SeatCrudController.php
@@ -2,24 +2,66 @@
 
 namespace App\Controller\Admin;
 
+
 use App\Entity\Seat;
+use App\Message\GenerateSeatsMessage;
+use App\MessageHandler\GenerateSeatsMessageHandler;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class SeatCrudController extends AbstractCrudController
 {
+    public function __construct(private AdminUrlGenerator $adminUrlGenerator, private MessageBusInterface $bus)
+    {
+    }
+
     public static function getEntityFqcn(): string
     {
         return Seat::class;
     }
 
-    /*
+    public function configureActions(Actions $actions): Actions
+    {
+
+        $generate = Action::new('generate')
+            ->linkToCrudAction('generate')
+            ->createAsGlobalAction()
+            ->setIcon('fa fa-star');
+
+
+        return parent::configureActions($actions)
+            ->add(Crud::PAGE_INDEX, $generate);
+
+    }
+
+    public function generate(AdminContext $context)
+    {
+        $msg = new GenerateSeatsMessage();
+        $this->bus->dispatch($msg);
+        $this->addFlash('success', 'Generation in progress');
+        $url = $this->adminUrlGenerator
+            ->setController(self::class)
+            ->setAction(Crud::PAGE_INDEX);
+        return $this->redirect($url);
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+//        return [
+//            IdField::new('id'),
+//            NumberField::new('number'),
+//            AssociationField::new(Screen::class),
+//        ];
+        yield AssociationField::new('screen');
+        yield IntegerField::new('number');
     }
-    */
+
 }
