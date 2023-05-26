ea49b2636e04584eb8ed03199d6d0f625f400af3
jagodakosinska <jagoda.kosinska87@gmail.com>
2023-05-26 10:55:43 +0200

    feat(Messenger): create message && handler to handle message for generating seats

---
+++ src/MessageHandler/GenerateSeatsMessageHandler.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\MessageHandler;
+
+use App\Entity\Screen;
+use App\Entity\Seat;
+use App\Message\GenerateSeatsMessage;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+
+final class GenerateSeatsMessageHandler implements MessageHandlerInterface
+{
+
+    public function __construct(private EntityManagerInterface $em)
+    {
+    }
+
+    public function __invoke(GenerateSeatsMessage $message)
+    {
+        $screens = $this->em->getRepository(Screen::class)->findAll();
+        foreach ($screens as $screen) {
+            $seatsNumbers = $screen->getNumberOfSeats();
+            $cnt = $this->em->getRepository(Seat::class)->count(['screen' => $screen]);
+            $toGenerate = $seatsNumbers - $cnt;
+            for ($i = 1; $i <= $toGenerate; $i++) {
+                $seat = (new Seat())->setScreen($screen)->setNumber($cnt + $i);
+                $this->em->persist($seat);
+            }
+        }
+        $this->em->flush();
+    }
+}
