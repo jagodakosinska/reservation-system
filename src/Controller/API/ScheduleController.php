eaeea22c5aaa989933a618c0385ce96520d0cbe1
jagodakosinska <jagoda.kosinska87@gmail.com>
2023-06-07 15:38:06 +0200

    feat(ScheduleController): add list of available seats to schedule

--- src/Controller/API/ScheduleController.php
+++ src/Controller/API/ScheduleController.php
@@ -4,12 +4,13 @@ namespace App\Controller\API;
 
 use App\Entity\Schedule;
 use App\Repository\ScheduleRepository;
+use App\Repository\SeatRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ScheduleController extends AbstractController
 {
-    #[Route('/api/schedule', name: 'schedules')]
+    #[Route('/api/schedule', name: 'all_schedules')]
     public function getAllSchedules(ScheduleRepository $scheduleRepository)
     {
         $res = ['schedule' => $scheduleRepository->listAllEvailable()];
@@ -17,10 +18,12 @@ class ScheduleController extends AbstractController
     }
 
     #[Route('api/schedule/{id}', name: 'schedule')]
-    public function getOneById(Schedule $schedule, ScheduleRepository $scheduleRepository)
+    public function getOneById(Schedule $schedule, SeatRepository $seatRepository)
     {
-        $res = ['schedule' => $scheduleRepository->find($schedule->getId())];
-        return $this->json($res, 200,[], ['groups' => ['schedule:read']]);
+        $list = $seatRepository->getAvailableBySchedule($schedule->getId(), $schedule->getScreen()->getId());
+        $res = ['schedule' => $schedule,
+            'seats' => $list];
+        return $this->json($res, 200,[], ['groups' => ['schedule:read','schedule:reservation']]);
     }
 
 }
