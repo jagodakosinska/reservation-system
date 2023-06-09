8dfe9c188ba64d8551eed8f3c934fe933d43f02a
jagodakosinska <jagoda.kosinska87@gmail.com>
2023-06-09 13:35:23 +0200

    refactor(ScheduleController): refactor routes, add types

--- src/Controller/API/ScheduleController.php
+++ src/Controller/API/ScheduleController.php
@@ -6,19 +6,21 @@ use App\Entity\Schedule;
 use App\Repository\ScheduleRepository;
 use App\Repository\SeatRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/api/schedule', name: 'schedule_', methods: 'GET')]
 class ScheduleController extends AbstractController
 {
-    #[Route('/api/schedule', name: 'all_schedules')]
-    public function getAllSchedules(ScheduleRepository $scheduleRepository)
+    #[Route('/', name: 'list')]
+    public function getAll(ScheduleRepository $scheduleRepository): Response
     {
-        $res = ['schedule' => $scheduleRepository->listAllEvailable()];
+        $res = ['schedule' => $scheduleRepository->listAllAvailable()];
         return $this->json($res);
     }
 
-    #[Route('api/schedule/{id}', name: 'schedule')]
-    public function getOneById(Schedule $schedule, SeatRepository $seatRepository)
+    #[Route('/{id}', name: 'show')]
+    public function getOneById(Schedule $schedule, SeatRepository $seatRepository): Response
     {
         $list = $seatRepository->getAvailableBySchedule($schedule->getId(), $schedule->getScreen()->getId());
         $res = ['schedule' => $schedule,
