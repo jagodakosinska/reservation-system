8b692b9619b6108d42cba43e35606e44b53cc521
jagodakosinska <jagoda.kosinska87@gmail.com>
2023-06-09 13:39:34 +0200

    refactor(ReservationItemRepository): remove unused, commented methods

--- src/Repository/ReservationItemRepository.php
+++ src/Repository/ReservationItemRepository.php
@@ -38,29 +38,4 @@ class ReservationItemRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
-
-//    /**
-//     * @return ReservationItem[] Returns an array of ReservationItem objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('r')
-//            ->andWhere('r.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('r.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?ReservationItem
-//    {
-//        return $this->createQueryBuilder('r')
-//            ->andWhere('r.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
