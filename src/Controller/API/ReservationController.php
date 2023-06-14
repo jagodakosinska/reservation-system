233633d8e239378d05ac54cb74c065b5696e3745
jagodakosinska <jagoda.kosinska87@gmail.com>
2023-06-14 10:09:50 +0200

    refactor(ReservationController): use new validation in controller instead of old one

--- src/Controller/API/ReservationController.php
+++ src/Controller/API/ReservationController.php
@@ -2,12 +2,16 @@
 
 namespace App\Controller\API;
 
+use App\DTO\ReservationDTO;
 use App\Repository\ReservationRepository;
-use App\RequestValidator\ReservationRequest;
+use App\Service\ErrorValidationService;
 use App\Service\ReservationService;
+use App\Validator\ReservationConstraint;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/api/reservation', name: 'reservation_')]
 class ReservationController extends AbstractController
@@ -21,9 +25,25 @@ class ReservationController extends AbstractController
     }
 
     #[Route('/create', name: 'new')]
-    public function create(ReservationRequest $request, ReservationService $reservationService): Response
+    public function create(
+        Request                $request,
+        ValidatorInterface     $validator,
+        ReservationConstraint  $constraints,
+        ErrorValidationService $errorValidationService,
+        ReservationService     $reservationService
+    ): Response
     {
-        $reservationService->handle($request);
+        $reservationDTO = ReservationDTO::fromRequest($request->toArray());
+        $errors = $validator->validate(value: $reservationDTO);
+
+        if ($errors->count() == 0) {
+            $errors = $validator->validate(value: $reservationDTO, constraints: $constraints);
+        }
+        if ($errors->count()) {
+            return $errorValidationService->handle($errors);
+        }
+
+        $reservationService->handle($reservationDTO);
 
         return $this->json(['status' => 'success']);
 
