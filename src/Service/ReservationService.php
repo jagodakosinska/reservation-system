46974efcaa9f7cb1a21b49eab16a5554382859ca
jagodakosinska <jagoda.kosinska87@gmail.com>
2023-06-14 10:03:05 +0200

    refactor(ReservationRequest): remove custom validator for replacing to standard symfony validator component

--- src/Service/ReservationService.php
+++ src/Service/ReservationService.php
@@ -7,30 +7,23 @@ use App\Entity\ReservationItem;
 use App\Entity\Schedule;
 use App\Entity\Seat;
 use App\Repository\PriceRepository;
-use App\Repository\ReservationItemRepository;
 use App\Repository\ReservationRepository;
-use App\Repository\ScheduleRepository;
-use App\Repository\SeatRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 
 
 class ReservationService
 {
     public function __construct(
         private EntityManagerInterface    $em,
-        private ScheduleRepository        $scheduleRepository,
         private ReservationRepository     $reservationRepository,
         private PriceRepository           $priceRepository,
-        private ReservationItemRepository $reservationItemRepository,
-        private SeatRepository            $seatRepository
     )
     {
     }
 
     public function handle($request): void
     {
-        $this->validate($request);
+
         $reservation = new Reservation();
         $reservation->setStatus(Reservation::RESERVED);
         $reservation->setSchedule($this->em->getReference(Schedule::class, $request->getScheduleId()));
@@ -40,34 +33,14 @@ class ReservationService
         $this->em->flush();
     }
 
-    public function validate($request): void
-    {
-        $schedule = $this->scheduleRepository->find($request->getScheduleId());
-        if (null === $schedule) {
-            throw new BadRequestException(sprintf("Schedule %d doesn't exist!", $request->getScheduleId()));
-        }
-
-        $seatList = array_column($request->getReservationItems(), 'seat');
-        $hasSeats = $this->seatRepository->hasSeats($schedule->getScreen()->getId(), $seatList);
-        if (false === $hasSeats) {
-            throw new BadRequestException("Seat doesn't exist!");
-        }
-
-        $isReserved = $this->reservationItemRepository->checkIsReserved($schedule->getId(), $seatList);
-
-        if ($isReserved) {
-            throw new BadRequestException("Seat is reserved!");
-        }
-    }
-
     private function prepareReservationItems($reservationItems, Reservation $reservation): void
     {
         $prices = $this->priceRepository->findAllByName();
 
         foreach ($reservationItems as $item) {
             $reservationItem = (new ReservationItem())
-                ->setSeat($this->em->getReference(Seat::class, $item['seat']))
-                ->setPrice($prices[$item['ticketType']]);
+                ->setSeat($this->em->getReference(Seat::class, $item->getSeat()))
+                ->setPrice($prices[$item->getTicketType()]);
             $reservation->addReservationItem($reservationItem);
         }
     }
