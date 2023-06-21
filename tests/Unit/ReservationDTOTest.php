478096711a110c813196598944fc13d5d14effe4
jagodakosinska <jagoda.kosinska87@gmail.com>
2023-06-21 15:47:29 +0200

    feat(PhpUnit): add asserts in unit tests

--- tests/Unit/ReservationDTOTest.php
+++ tests/Unit/ReservationDTOTest.php
@@ -3,7 +3,12 @@
 namespace App\Tests\Unit;
 
 use App\DTO\ReservationDTO;
+use App\DTO\ReservationItemsDTO;
 use PHPUnit\Framework\TestCase;
+
+/**
+ * @group unit
+ */
 
 class ReservationDTOTest extends TestCase
 {
@@ -16,7 +21,11 @@ class ReservationDTOTest extends TestCase
         $this->assertEquals(1, $dto->scheduleId);
         $this->assertCount(2, $dto->reservationItems);
         $this->assertInstanceOf(ReservationDTO::class, $dto);
-
+        $this->assertIsArray($dto->reservationItems);
+        $this->assertInstanceOf(ReservationItemsDTO::class, $dto->reservationItems[0]);
+        $this->assertIsString($dto->reservationItems[0]->ticketType);
+        $this->assertContains($dto->reservationItems[0]->ticketType, ['junior', 'adult']);
+        $this->assertIsInt($dto->reservationItems[0]->seat);
     }
 
     private function createFromJson()
