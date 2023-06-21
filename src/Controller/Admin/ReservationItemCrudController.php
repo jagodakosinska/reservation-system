b14a812df2726a546e8bb09f3cece08115915fe0
jagodakosinska <jagoda.kosinska87@gmail.com>
2023-06-21 15:51:30 +0200

    refactor: ReservationItemCrudController

--- src/Controller/Admin/ReservationItemCrudController.php
+++ src/Controller/Admin/ReservationItemCrudController.php
@@ -5,8 +5,6 @@ namespace App\Controller\Admin;
 use App\Entity\ReservationItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
-use PhpParser\Node\Expr\Yield_;
 
 class ReservationItemCrudController extends AbstractCrudController
 {
@@ -18,9 +16,8 @@ class ReservationItemCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-
-            yield AssociationField::new('seat');
-            yield AssociationField::new('price');
+        yield AssociationField::new('seat');
+        yield AssociationField::new('price');
     }
 
 }
